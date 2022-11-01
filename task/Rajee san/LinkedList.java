5032267010ffb8f6ca650cf215bf76855deab024
ZSGS-ANANTHIKARTHICK <112965529+Ananthi-karthick@users.noreply.github.com>
2022-11-01 17:18:23 +0530

    Update LinkedList.java

--- task/Rajee san/LinkedList.java
+++ task/Rajee san/LinkedList.java
@@ -1,29 +1,88 @@
 package day3test1;
 
-public class LinkedList {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-	public static void main(String[] args) {
-		int[] a = { 2, 7, 4, 3, 5 };
-		int n = a.length;
-		int max = 0;
-		int[] a1 = new int[n];
-		for (int i = 0; i < n - 1; i++) {
-			max = a[i];
-			for (int j = i + 1; j < n; j++) {
-				if (max < a[j]) {
-					max = a[j];
-				}
+public class SinglyLinkedList {
+
+	class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public Node head = null;
+	public Node tail = null;
+
+	public void addNode(int data) { // 2
+
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public void display() {
+		Node current = head;
+
+		if (head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		System.out.println("Nodes of singly linked list: ");
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+	public void max() {
+
+		Node max = head;
+		Node j = head.next;
+		while (max != tail) {
+			int n = max.data;
+			while (j != null) {
+				if (max.data < j.data)
+					max.data = j.data;
+
+				j = j.next;
 
 			}
-			if (max != a[i])
-				a1[i] = max;
-			else
-				a1[i] = 0;
+			if (max.data == n) {
+				max.data = 0;
+			}
+			max = max.next;
+			j = max.next;
 		}
-		a1[n - 1] = 0;
-		for (int x : a1) {
-			System.out.print(x + ",");
-		}
+		tail.data = 0;
+		display();// 7
+	}
+
+	public static void main(String[] args) {
+
+		SinglyLinkedList sList = new SinglyLinkedList();
+
+		// Add nodes to the list
+		sList.addNode(2);
+		sList.addNode(7);
+		sList.addNode(4);
+		sList.addNode(3);
+		sList.addNode(5);
+		// Displays the nodes present in the list
+		sList.display();
+		sList.max();
 	}
 
 }
