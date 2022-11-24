9f4a45452181c9194e086f9a1bc212627ba705dc
ZSGS-ANANTHIKARTHICK <112965529+Ananthi-karthick@users.noreply.github.com>
2022-11-24 14:36:38 +0530

    Add files via upload

---
+++ task/Rajee san/LongestCommonString.java
@@ -0,0 +1,35 @@
+package day7;
+
+//input
+//["flower","flow","flight"]
+
+//output
+//fl 2
+public class LongestCommonString {
+
+	
+		public static String longestCommonPrefix(String[] string) {
+			if (string.length == 0) 
+				return "";
+			String prefix = string[0];System.out.println(prefix);
+			for (int i = 1; i < string.length; i++) {
+				while (string[i].indexOf(prefix) != 0) {
+					prefix = prefix.substring(0, prefix.length() - 1);
+					
+					if (prefix.isEmpty()) 
+						return "";
+					
+				}
+				System.out.println(prefix);
+			}
+			
+			return prefix;
+		}
+	public static void main(String[] args) {
+	String[] string= {"Flower","Flow","Flight"};
+            String longestCommonString=longestCommonPrefix(string);
+            System.out.println(longestCommonString+"-"+longestCommonString.length());
+	}
+}
+
+
